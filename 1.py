5c31c7b6b0540aa494ddffa2e71072cbc5efe67c
unik1-web <75004740+unik1-web@users.noreply.github.com>
2025-02-07 07:24:46 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,35 @@
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.
+#
+# Input Format
+#
+# The first line contains . The second line contains an array   of  integers each separated by a space.
+#
+# Constraints
+#
+# Output Format
+#
+# Print the runner-up score.
+#
+# Sample Input 0
+#
+# 5
+# 2 3 6 6 5
+# Sample Output 0
+#
+# 5
+# Explanation 0
+#
+# Given list is . The maximum score is , second maximum is . Hence, we print  as the runner-up score.
+from operator import index
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr=list(arr)
+    max_ = max(arr)
+    arr = set(arr)
+    arr.discard(max_)
+    arr = list(arr)
+    max_=max(arr)
+    print(max_)
 
